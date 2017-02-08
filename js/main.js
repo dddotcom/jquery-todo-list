2743b1ebd337e537762bcee4b5bb8c5a174a1cb7
dddotcom <dddotcom@gmail.com>
2017-02-07 21:53:55 -0800

    added to do list functionality w/ jquery; added sortable library; list saves to local storage

---
+++ js/main.js
@@ -0,0 +1,114 @@
+var LOCAL_STORAGE_KEY = "toDoList";
+var todoDescriptions = [];
+
+$(document).ready(function() {
+  console.log("DOM loaded");
+  //load things from local storage
+  displayItemsFromLocalStorage();
+
+  //prevent page from refreshing when enter is pressed
+  $('#addTodoForm').submit(function(e){
+      e.preventDefault();
+ });
+
+  $("#add-button").click(addToDoItem);
+
+  $("#todo-list").on('click', 'button', removeToDoItem)
+
+  $("#todo-list").sortable();
+  $("#todo-list").disableSelection();
+
+})
+
+function removeToDoItem() {
+
+  //remove the todo-item
+  var todoItem = $(this).parent().parent();
+  todoItem.remove();
+
+  //get the toDoItemDescription that you are removing
+  //remove from local storage
+  var removeItem = $(this).parent().parent().children(".todo-desc").children("p").text();
+  removeFromLocalStorage(  removeItem );
+
+  $('#error-msg').css('visibility', 'visible').css('color', '#40bf79');
+  $('#error-msg').html("Crossed " + removeItem + " off the list!");
+
+}
+
+function addToDoItem() {
+  //only add if there is a value there
+  if($("#newtodo").val()){
+    var newToDo = $("#newtodo").val();
+
+    //create new todo item
+    var newItem = $("<div>").addClass("todo-item");
+    var newItemDesc = $("<div>").addClass("todo-desc");
+    var newRemove = $("<div>").addClass("todo-remove");
+    var newButton = $("<button>").addClass("remove-button").text("Remove");
+    var newDesc = $("<p>").text(newToDo);
+
+    //add it to the todo list
+    newItemDesc.append(newDesc);
+    newRemove.append(newButton);
+    newItem.append(newItemDesc);
+    newItem.append(newRemove);
+    $("#todo-list").append(newItem)
+
+    $('#error-msg').css('visibility', 'visible').css('color', '#40bf79');
+    $('#error-msg').html("Added " + newToDo + " to the list!");
+
+    //add to local storage
+    addToLocalStorage(newToDo);
+
+    //reset input field
+    resetInputField();
+  } else {
+    $('#error-msg').css('visibility', 'visible');
+  }
+
+}
+
+function resetInputField() {
+  $("#newtodo").val('');
+  $("#newtodo").focus();
+}
+
+function displayItemsFromLocalStorage(){
+  if(localStorage.getItem(LOCAL_STORAGE_KEY)){
+      todoDescriptions = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+      for(var i = 0; i < todoDescriptions.length; i++){
+
+        //create new todo item
+        var newItem = $("<div>").addClass("todo-item");
+        var newItemDesc = $("<div>").addClass("todo-desc");
+        var newRemove = $("<div>").addClass("todo-remove");
+        var newButton = $("<button>").addClass("remove-button").text("Remove");
+        var newDesc = $("<p>").addClass("todo-remove").text(todoDescriptions[i]);
+
+        //add it to the todo list
+        newItemDesc.append(newDesc);
+        newRemove.append(newButton);
+        newItem.append(newItemDesc);
+        newItem.append(newRemove);
+        $("#todo-list").append(newItem)
+      }
+  }
+}
+
+function addToLocalStorage(todoDesc){
+  todoDescriptions.push(todoDesc);
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todoDescriptions));
+}
+
+function removeFromLocalStorage(todoDesc){
+  //get index of description
+  var index = todoDescriptions.indexOf(todoDesc);
+
+  //delete that item from local storage
+  todoDescriptions.splice(index, 1);
+
+  //update local storage
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todoDescriptions));
+}
